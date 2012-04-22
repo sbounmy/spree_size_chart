9c31e72b8b13b8ff38c24ce7a45376378bc4a9c7
Bounmy Stephane <stephanebounmy@gmail.com>
2012-04-22 21:51:06 +0200

    display size chart admin sidebar

--- app/overrides/add_product_tab.rb
+++ app/overrides/add_product_tab.rb
@@ -1,6 +1,6 @@
 Deface::Override.new( :virtual_path => "spree/admin/shared/_product_tabs",
                       :name => "add_size_chart_product_tabs",
                       :insert_bottom => 'ul[data-hook="admin_product_tabs"].sidebar.product-menu',
-                      :text => %Q{<li<%== ' class="active"' if current == 'SizeChart' %>>
+                      :text => %Q{<li<%== ' class="active"' if current == 'Size Chart' %>>
                         <%= link_to t(:size_chart), edit_admin_product_size_chart_url(@product) %>
                       </li>})
