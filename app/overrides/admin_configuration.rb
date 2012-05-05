75f09f0f2e1944cce30f0e5bc10a67db9562262f
Bounmy Stephane <stephanebounmy@gmail.com>
2012-05-05 10:44:41 +0200

    fixing duplicate conflict when overriding

--- app/overrides/admin_configuration.rb
+++ app/overrides/admin_configuration.rb
@@ -8,6 +8,11 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
 Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
                     :name => "add_size_types_to_configuration_menu",
                     :insert_bottom => "[data-hook='admin_configurations_menu']",
-                    :partial => "spree/admin/shared/configurations_menu",
+                    :text => %q{
+                      <tr>
+                        <td><%= link_to t("size_types"), admin_size_types_path %></td>
+                        <td><%= t("size_types_description") %></td>
+                      </tr>
+                    },
                     :original => '2645fce66adc090d7ce0aa34f7cdfcaabf1be9f0',
                     :disabled => false)
