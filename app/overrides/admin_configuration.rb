8a37086b0d3b4f0e0998765b78ad8635accfd1c0
Bounmy Stephane <stephanebounmy@gmail.com>
2012-07-21 19:10:11 +0200

    added link to configuration

--- app/overrides/admin_configuration.rb
+++ app/overrides/admin_configuration.rb
@@ -16,3 +16,15 @@ Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
                     },
                     :original => '2645fce66adc090d7ce0aa34f7cdfcaabf1be9f0',
                     :disabled => false)
+
+
+Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
+                    :name => "add_size_prototypes_to_configuration_menu",
+                    :insert_bottom => "[data-hook='admin_configurations_menu']",
+                    :text => %q{
+                      <tr>
+                        <td><%= link_to t("size_prototypes"), admin_size_prototypes_path %></td>
+                        <td><%= t("size_prototypes_description") %></td>
+                      </tr>
+                    },
+                    :disabled => false)
