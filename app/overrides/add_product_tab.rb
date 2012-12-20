e682bd904840203e23b133fe8a84e40beca54a30
Stephane Bounmy <stephanebounmy@gmail.com>
2012-12-20 21:17:55 +0100

    fixed product_tabs override

--- app/overrides/add_product_tab.rb
+++ app/overrides/add_product_tab.rb
@@ -1,6 +1,6 @@
 Deface::Override.new( :virtual_path => "spree/admin/shared/_product_tabs",
                       :name => "add_size_chart_product_tabs",
-                      :insert_bottom => 'ul[data-hook="admin_product_tabs"].sidebar.product-menu',
+		      :insert_bottom => '[data-hook="admin_product_tabs"]',
                       :text => %Q{<li<%== ' class="active"' if current == 'Size Chart' %>>
                         <%= link_to t(:size_chart), edit_admin_product_size_chart_url(@product) %>
                       </li>})
