9c3f31a0073aa8c836dd2db75e7e0736dba1c832
Stephane Bounmy <stephanebounmy@gmail.com>
2012-12-25 19:31:06 +0100

    added icon to size chart link

--- app/overrides/add_product_tab.rb
+++ app/overrides/add_product_tab.rb
@@ -2,5 +2,5 @@ Deface::Override.new( :virtual_path => "spree/admin/shared/_product_tabs",
                       :name => "add_size_chart_product_tabs",
 		      :insert_bottom => '[data-hook="admin_product_tabs"]',
                       :text => %Q{<li<%== ' class="active"' if current == 'Size Chart' %>>
-                        <%= link_to t(:size_chart), edit_admin_product_size_chart_url(@product) %>
+                        <%= link_to_with_icon 'icon-table', t(:size_chart), edit_admin_product_size_chart_url(@product) %>
                       </li>})
