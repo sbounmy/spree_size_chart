0d2f63fb6aee0896c7d09dd0ce6a89c304951769
Stephane Bounmy <stephanebounmy@gmail.com>
2013-02-26 23:21:37 +0100

    fix : size chart controller

--- app/controllers/spree/admin/size_charts_controller.rb
+++ app/controllers/spree/admin/size_charts_controller.rb
@@ -1,23 +1,25 @@
 module Spree
   module Admin
     class SizeChartsController < ResourceController
+      belongs_to 'spree/product', :find_by => :permalink
 
       def edit
-        @product = Product.find_by_permalink params[:product_id]
-        @size_chart = @product.size_chart || @product.build_size_chart
       end
 
       def update
-        @product = Product.find_by_permalink params[:product_id]
-        if (@product.size_chart || @product.build_size_chart).update_attributes params[:size_chart]
+        if @size_chart.update_attributes params[:size_chart]
           flash[:notice] = "Size Chart successfully updated"
         else
         end
-        redirect_to edit_admin_product_size_chart_path(@product)
+        redirect_to edit_admin_product_size_chart_path(parent)
       end
 
       def collection_url
-        admin_product_size_chart_url(@product)
+        admin_product_size_chart_url(parent)
+      end
+
+      def load_resource_instance
+        @size_chart ||= parent.size_chart || parent.build_size_chart
       end
     end
   end
