5badade2f79a83e0d40cdd21e020a19e3a97fee2
Stephane Bounmy <stephanebounmy@gmail.com>
2013-04-13 21:10:42 +0200

    fixed migration

--- db/migrate/20130413171901_move_size_prototype_to_spree_prototypes.rb
+++ db/migrate/20130413171901_move_size_prototype_to_spree_prototypes.rb
@@ -1,23 +1,25 @@
 class MoveSizePrototypeToSpreePrototypes < ActiveRecord::Migration
   def up
-    if defined? Spree::SizePrototype
-      prototypes = {}
-      # For each size prototype create its corresponding Spree::Prototype by prefixing name 'SizeChart : %{name}'
-      Spree::SizePrototype.all.each do |prototype|
-        say "Creating Spree::Prototype 'SizeChart : #{prototype.name}'"
-        spree_prototype = Spree::Prototype.create(:name => "SizeChart : #{prototype.name}", :size_chart_image => prototype.image)
-
-        prototypes[prototype.id] = spree_prototype.id
-      end
-
-      # move size chart prototype to newly create Spree::Prototype
-      Spree::SizeChart.where('size_prototype_id IS NOT NULL').each do |chart|
-        say "Updating Size chart##{chart.id}..."
-        chart.update_attribute :size_prototype_id, prototypes[chart.size_prototype_id]
-      end
-
-      rename_column :spree_size_charts, :size_prototype_id, :prototype_id
+    class SizePrototype < ActiveRecord::Base
+      has_one :image, :as => :viewable
     end
+
+    prototypes = {}
+    # For each size prototype create its corresponding Spree::Prototype by prefixing name 'SizeChart : %{name}'
+    Spree::SizePrototype.all.each do |prototype|
+      say "Creating Spree::Prototype 'SizeChart : #{prototype.name}'"
+      spree_prototype = Spree::Prototype.create(:name => "SizeChart : #{prototype.name}", :size_chart_image => prototype.image)
+
+      prototypes[prototype.id] = spree_prototype.id
+    end
+
+    # move size chart prototype to newly create Spree::Prototype
+    Spree::SizeChart.where('size_prototype_id IS NOT NULL').each do |chart|
+      say "Updating Size chart##{chart.id}..."
+      chart.update_attribute :size_prototype_id, prototypes[chart.size_prototype_id]
+    end
+
+    rename_column :spree_size_charts, :size_prototype_id, :prototype_id
   end
 
 end
