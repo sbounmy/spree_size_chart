209070f258d0ffc71b4ced0bca3e2756e6f19e60
Stephane Bounmy <stephanebounmy@gmail.com>
2013-04-14 09:59:30 +0200

    moved size types to product sub tabs

--- app/overrides/admin_configuration.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
-                     :name => "add_size_chart_type_link_configuration_menu",
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text => %q{<%= configurations_sidebar_menu_item t("size_types"), admin_size_types_path %>},
-                     :disabled => false)
